8fc15fe3f147abc4ec6d55310d519f024ff42ca3
Kolibrzyk <131911615+kolibrzyk@users.noreply.github.com>
2025-09-28 14:22:46 +0200

    Update DropAfterKillPlugin.cs

--- DropAfterKillPlugin.cs
+++ DropAfterKillPlugin.cs
@@ -4,6 +4,8 @@ using CounterStrikeSharp.API.Modules.Events;
 using CounterStrikeSharp.API.Modules.Utils;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace DropAfterKillPlugin
 {
@@ -13,70 +15,152 @@ namespace DropAfterKillPlugin
         public override string ModuleVersion => "1.0.0";
         public override string ModuleAuthor => "YourName";
 
-        // List of possible rewards
-        private readonly List<string> rewards = new()
-        {
-            "Extra $1000",
-            "50 HP Heal",
-            "Kevlar + Helmet",
-            "Random Grenade",
-            "Double Jump for 10s"
-        };
-
-        // Random generator
-        private readonly Random rng = new();
-
-        // Drop chance (percentage)
-        private const int DropChancePercent = 30; // 30% chance
+        private Random rng = new();
+        private PluginConfig config;
 
         public override void Load(bool hotReload)
         {
+            LoadConfig();
             RegisterEventHandler<EventPlayerDeath>(OnPlayerDeath);
+        }
+
+        private void LoadConfig()
+        {
+            string path = Path.Combine(PluginPath, "config.json");
+            if (!File.Exists(path))
+            {
+                PrintToConsole("[DropAfterKill] config.json not found, creating default...");
+                config = PluginConfig.Default();
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+                return;
+            }
+
+            config = JsonConvert.DeserializeObject<PluginConfig>(File.ReadAllText(path)) ?? PluginConfig.Default();
         }
 
         private HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info)
         {
-            if (@event.Attacker == null || @event.Attacker.SteamID == 0)
+            if (@event.Attacker == null || !@event.Attacker.IsValid || @event.Attacker.Team == CsTeam.Spectator)
                 return HookResult.Continue;
 
-            CCSPlayerController attacker = @event.Attacker;
+            var attacker = @event.Attacker;
 
-            if (!attacker.IsValid || attacker.Team == CsTeam.Spectator)
-                return HookResult.Continue;
-
-            // Roll for drop chance
             int roll = rng.Next(0, 100);
-            if (roll < DropChancePercent)
-            {
-                string reward = rewards[rng.Next(rewards.Count)];
+            if (roll >= config.DropChancePercent) return HookResult.Continue;
 
-                attacker.PrintToChat($" \x07[Drop System]\x01 You received: \x04{reward}\x01!");
+            if (config.Rewards.Count == 0) return HookResult.Continue;
 
-                // Reward handling
-                switch (reward)
-                {
-                    case "Extra $1000":
-                        attacker.InGameMoneyServices.Account += 1000;
-                        break;
-                    case "50 HP Heal":
-                        if (attacker.PlayerPawn.Value != null)
-                            attacker.PlayerPawn.Value.Health += 50;
-                        break;
-                    case "Kevlar + Helmet":
-                        attacker.GiveNamedItem("item_kevlar");
-                        attacker.GiveNamedItem("item_assaultsuit");
-                        break;
-                    case "Random Grenade":
-                        string[] nades = { "weapon_hegrenade", "weapon_flashbang", "weapon_smokegrenade", "weapon_molotov" };
-                        attacker.GiveNamedItem(nades[rng.Next(nades.Length)]);
-                        break;
-                    case "Double Jump for 10s":
-                        // Placeholder for future ability system
-                        break;
-                }
-            }
+            var reward = config.Rewards[rng.Next(config.Rewards.Count)];
+            ApplyReward(attacker, reward);
 
             return HookResult.Continue;
         }
+
+        private void ApplyReward(CCSPlayerController player, Reward reward)
+        {
+            switch (reward.Type.ToLower())
+            {
+                case "cash":
+                    player.InGameMoneyServices.Account += reward.Amount;
+                    player.PrintToChat($"[Drop System] You received: ${reward.Amount}!");
+                    break;
+
+                case "hp":
+                    if (player.PlayerPawn.Value != null)
+                    {
+                        player.PlayerPawn.Value.Health += reward.Amount;
+                        player.PrintToChat($"[Drop System] You received: +{reward.Amount} HP!");
+                    }
+                    break;
+
+                case "armor":
+                    if (reward.Kevlar) player.GiveNamedItem("item_kevlar");
+                    if (reward.Helmet) player.GiveNamedItem("item_assaultsuit");
+                    player.PrintToChat("[Drop System] You received: Armor!");
+                    break;
+
+                case "grenade":
+                    if (reward.List != null && reward.List.Count > 0)
+                    {
+                        string grenade = reward.List[rng.Next(reward.List.Count)];
+                        player.GiveNamedItem(grenade);
+                        player.PrintToChat($"[Drop System] You received: {grenade}!");
+                    }
+                    break;
+
+                case "speed":
+                    player.ApplySpeedBoost(reward.Multiplier, reward.Duration);
+                    player.PrintToChat($"[Drop System] You received: Speed x{reward.Multiplier} for {reward.Duration}s!");
+                    break;
+
+                case "bhop":
+                    player.ApplyBunnyhop(reward.Duration);
+                    player.PrintToChat($"[Drop System] You received: Bunnyhop for {reward.Duration}s!");
+                    break;
+
+                case "vip":
+                    int vipMinutes = rng.Next(reward.MinMinutes, reward.MaxMinutes + 1);
+                    player.ApplyVIP(vipMinutes);
+                    player.PrintToChat($"[Drop System] You received: VIP for {vipMinutes} minutes!");
+                    break;
+
+                case "credits":
+                    player.AddCredits(reward.Amount);
+                    player.PrintToChat($"[Drop System] You received: {reward.Amount} credits!");
+                    break;
+
+                case "xp":
+                    player.AddXP(reward.Amount);
+                    player.PrintToChat($"[Drop System] You received: {reward.Amount} XP!");
+                    break;
+            }
+        }
+    }
+
+    // Classes for config deserialization
+    public class PluginConfig
+    {
+        public int DropChancePercent { get; set; }
+        public List<Reward> Rewards { get; set; } = new();
+
+        public static PluginConfig Default() => new PluginConfig
+        {
+            DropChancePercent = 30,
+            Rewards = new List<Reward>
+            {
+                new Reward { Type = "Cash", Amount = 1000 },
+                new Reward { Type = "HP", Amount = 50 },
+                new Reward { Type = "Armor", Kevlar = true, Helmet = true },
+                new Reward { Type = "Grenade", List = new List<string>{ "weapon_hegrenade", "weapon_flashbang", "weapon_smokegrenade", "weapon_molotov" } },
+                new Reward { Type = "Speed", Multiplier = 1.2f, Duration = 30 },
+                new Reward { Type = "Bhop", Duration = 60 },
+                new Reward { Type = "VIP", MinMinutes = 5, MaxMinutes = 1440 },
+                new Reward { Type = "Credits", Amount = 50 },
+                new Reward { Type = "XP", Amount = 100 }
+            }
+        };
+    }
+
+    public class Reward
+    {
+        public string Type { get; set; } = "";
+        public int Amount { get; set; } = 0;
+        public bool Kevlar { get; set; } = false;
+        public bool Helmet { get; set; } = false;
+        public List<string>? List { get; set; }
+        public float Multiplier { get; set; } = 1f;
+        public int Duration { get; set; } = 0;
+        public int MinMinutes { get; set; } = 5;
+        public int MaxMinutes { get; set; } = 1440;
+    }
+
+    // Extension methods for abilities (placeholders, implement as needed)
+    public static class PlayerExtensions
+    {
+        public static void ApplySpeedBoost(this CCSPlayerController player, float multiplier, int duration) { /* TODO */ }
+        public static void ApplyBunnyhop(this CCSPlayerController player, int duration) { /* TODO */ }
+        public static void ApplyVIP(this CCSPlayerController player, int minutes) { /* TODO */ }
+        public static void AddCredits(this CCSPlayerController player, int amount) { /* TODO */ }
+        public static void AddXP(this CCSPlayerController player, int amount) { /* TODO */ }
     }
 }
